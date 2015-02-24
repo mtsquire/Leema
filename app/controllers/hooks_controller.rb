81ba913b2550fd94a3efa5774846e60d7768fd8e
Brandon Hay <brandon.a.hay@gmail.com>
2015-02-23 21:26:29 -0500

    Testing the webhooks controller

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,12 +4,7 @@ class HooksController < ApplicationController
       when 'balance.available'
         #Transfer money to supplier bank account after balance is available.
         puts "Initiating Stripe transfer"
-        transfer = Stripe::Transfer.create(
-          #Take 10% for ourselves
-          :amount => (product.price * 90).floor,
-          :currency => "usd",
-          :recipient => product.suppliers.first.token
-        )
+        puts "Hey this webhook worked"
       end
   end
 end
