9b32e9fe42fa9c451b33a3066b1b6f8a64cc903f
msquire <marc.squire@mlb.com>
2015-02-27 15:21:26 -0500

    switching layout to full width and making talons drop downs

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -29,10 +29,12 @@ Spree::BaseHelper.module_eval do
 
   def taxons_tree(root_taxon, current_taxon, max_level = 1)
     return '' if max_level < 1 || root_taxon.children.empty?
-    content_tag :div, class: 'list-group' do
+    content_tag :ul, class: 'dropdown-menu', role: 'menu' do
       root_taxon.children.map do |taxon|
-        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
-        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+        content_tag :li do
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'active' : ''
+          link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+        end
       end.join("\n").html_safe
     end
   end
