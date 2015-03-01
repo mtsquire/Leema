34679c821cd228476f1f0af7347974ebfcea97d7
Brandon Hay <brandon.a.hay@gmail.com>
2015-03-01 12:00:26 -0500

    bypass CSRF authenticity error

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,7 @@
 class HooksController < ApplicationController
+# to bypass CSRF token authenticity error
+skip_before_filter  :verify_authenticity_token
+
   def stripe
     case params[:type]
       when 'balance.available'
