0b44b10b66b366b809da5fb9b5b0faf3684e2e18
Brandon Hay <brandon.a.hay@gmail.com>
2015-03-18 19:03:18 -0400

    deleted the per method to fix error

---
+++ app/controllers/spree/admin/promotions_controller_decorator.rb
@@ -0,0 +1,16 @@
+Spree::Admin::PromotionsController.class_eval do
+  protected
+  def collection
+    return @collection if defined?(@collection)
+    params[:q] ||= HashWithIndifferentAccess.new
+    params[:q][:s] ||= 'id desc'
+
+    @collection = super
+    @search = @collection.ransack(params[:q])
+    @collection = @search.result(distinct: true).
+      includes(promotion_includes).
+      page(params[:page])
+
+    @collection
+  end
+end
