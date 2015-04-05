42676612fd5cb733234c20723b8cfcce4f685597
Brandon Hay <brandon.a.hay@gmail.com>
2015-04-04 21:44:28 -0700

    fixed order instance var

--- app/controllers/my_orders_controller.rb
+++ app/controllers/my_orders_controller.rb
@@ -1,6 +1,6 @@
 class MyOrdersController < ApplicationController
 	layout "my_orders"
   def index
-    @orders = Spree::Order.where(["user_id = ?", current_user.id]).where(["completed_at != ?", nil])
+    @orders = Spree::Order.where(["user_id = ?", current_user.id]).where(["state = 'complete'"])
   end
 end
