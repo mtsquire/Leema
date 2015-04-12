d385d58d4cd68cd80ca25155657b9eabcaf63c0b
Brandon Hay <brandon.a.hay@gmail.com>
2015-04-12 18:34:03 -0400

    attempting to delete products and stock location if user is deleted

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,14 +1,36 @@
 Spree.user_class.class_eval do
 
-  belongs_to :supplier, class_name: 'Spree::Supplier'
+  belongs_to :supplier, class_name: 'Spree::Supplier', dependent: :destroy
 
   has_many :variants, through: :supplier
+
+  before_destroy :delete_products
+  before_destroy :delete_stock_location
+
   def admin?
     has_spree_role?("admin")
   end
   
   def supplier?
     supplier.present?
+  end
+
+  private
+
+  def delete_products
+    if self.supplier?
+      products = Spree::Product.where("user_id = ?", self.id)
+      if products
+        products.delete_all
+      end
+    end
+  end
+  def delete_stock_location
+    if self.supplier?
+      if self.supplier.stock_locations
+        self.supplier.stock_locations.delete_all
+      end
+    end
   end
 
 end
