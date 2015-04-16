d726e700530d101d8d93f52ad8b54718d430ec0c
msquire <marc.squire@mlb.com>
2015-04-16 17:28:28 -0400

    attempting to use s3_host_alias

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "https://d1vw1wxlid17s8.cloudfront.net"
+  config.action_controller.asset_host = "https:d1vw1wxlid17s8.cloudfront.net"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -88,8 +88,8 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-
-    url: ":s3_domain_url",
+    :s3_host_alias => 'https:d1vw1wxlid17s8.cloudfront.net',
+    url: ":s3_alias_url",
     path: "/:class/:attachment/:id_partition/:style/:filename"
   }
 
