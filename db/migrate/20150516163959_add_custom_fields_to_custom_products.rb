04bdfc908ba1ba2d2adb94b7e18bcf0ab121f814
Brandon Hay <brandon.a.hay@gmail.com>
2015-05-20 22:13:25 -0400

    modify migration files

--- db/migrate/20150516163959_add_custom_fields_to_custom_products.rb
+++
@@ -1,6 +0,0 @@
-class AddCustomFieldsToCustomProducts < ActiveRecord::Migration
-  def change
-    add_column :spree_products, :custom_message, :text
-    add_column :spree_products, :deliver_by_date, :date
-  end
-end
--- db/migrate/20150519222320_add_custom_fields_to_orders.rb
+++ db/migrate/20150519222320_add_custom_fields_to_orders.rb
@@ -2,7 +2,5 @@ class AddCustomFieldsToOrders < ActiveRecord::Migration
   def change
     add_column :spree_orders, :custom_message, :text
     add_column :spree_orders, :deliver_by_date, :date
-    remove_column :spree_products, :custom_message, :text
-    remove_column :spree_products, :deliver_by_date, :date
   end
 end
