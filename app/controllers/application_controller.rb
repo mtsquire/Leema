5718af690a02e20efb587a8a8a1b50def0e8df87
msquire <marc.squire@mlb.com>
2015-05-28 23:29:09 -0400

    adding predictive search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,27 @@ class ApplicationController < ActionController::Base
     store_location_for(:user, request.url)
   end
 
+  #predictive search shizzz
+  @product_names = []
+  @allProducts = Spree::Product.all
+  @allProducts.each do |product|
+    @product_names << product.name
+  end
+  File.open("public/productnames.json","w") do |f|
+    f.write(@product_names.to_json)
+  end
+
+  @supplier_names = []
+  @allSuppliers = Spree::Supplier.all
+  @allSuppliers.each do |supplier|
+    @supplier_names << supplier.store_name
+  end
+  File.open("public/suppliernames.json","w") do |f|
+    f.write(@supplier_names.to_json)
+  end
+
+
+
   protected
 
     #Added so a user can edit their profile page.
