ae74e89ee62d783bdb08b772ddc0eb1fd4e9cb8b
Brandon Hay <brandon.a.hay@gmail.com>
2015-05-31 18:24:29 -0400

    dont track variant stock

---
+++ app/models/spree/variant_decorator.rb
@@ -0,0 +1,14 @@
+Spree::Variant.class_eval do
+  after_create :remove_tracking, :unless => :is_master?
+  # def false
+  #   false
+  # end
+
+  def remove_tracking
+    byebug
+    # Remove the tracking requirement on new variants.
+    puts "variant is master? #{self.is_master?}"
+    self.track_inventory = false
+    puts "variant is tracking inventory? #{self.track_inventory}"
+  end
+end
