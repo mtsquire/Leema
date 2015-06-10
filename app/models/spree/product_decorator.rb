257339dd9961c83ac42e85223ff7825048371d71
Brandon Hay <brandon.a.hay@gmail.com>
2015-06-09 20:40:01 -0400

    adding new search query for suppliers

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,8 +7,9 @@ Spree::Product.class_eval do
 
   def self.search(search)
     if search
-      where(['name ILIKE ?', "%#{search}%"]) &&
-      where(['leema_description ILIKE ?', "%#{search}%" ])
+      where(['name LIKE ?', "%#{search}%"]) &&
+      where(['leema_description LIKE ?', "%#{search}%" ]) &&
+      self.joins(:suppliers).where('store_name ILIKE ?', '%' + '{search}' + '%')
     else
       @products = Spree::Product.all
     end
