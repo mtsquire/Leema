430301b4a6ae5a8f6dc1e120f1c4eaf59323dcd3
Brandon Hay <brandon.a.hay@gmail.com>
2015-06-21 19:47:58 -0400

    added prototype fields on product new page and set them to hidden. Wrote function for applying price increase to variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,13 +1,9 @@
 Spree::Variant.class_eval do
-  # after_create :remove_tracking, :unless => :is_master?
-  # # def false
-  # #   false
-  # # end
+  after_save :add_variant_price, :unless => :is_master?
 
-  # def remove_tracking
-  #   # Remove the tracking requirement on new variants.
-  #   puts "variant is master? #{self.is_master?}"
-  #   self.track_inventory = false
-  #   puts "variant is tracking inventory? #{self.track_inventory}"
-  # end
+  def add_variant_price
+    if product.price_increase && (product.price_increase_changed? || product.price_increase.new_record?)
+      self.price = product.master.price + product.price_increase
+    end
+  end
 end
