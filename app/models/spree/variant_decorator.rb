9d20bdcb8b42520ea71c66b0c4387de76afd1251
Brandon Hay <brandon.a.hay@gmail.com>
2015-06-26 13:46:39 -0400

    removed byebug references

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,11 +2,9 @@ Spree::Variant.class_eval do
   after_create :add_variant_price, :if => :okay_to_increase_variant_price?
 
   def add_variant_price
-    byebug
     puts 'self.is_master = #{self.is_master}'
     self.price = product.price_increase
     self.save!
-    byebug
   end
 
   def okay_to_increase_variant_price?
