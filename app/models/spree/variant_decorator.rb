66b204ff6482d9929023b1dd769cac72c8cb5c3f
Brandon Hay <brandon.a.hay@gmail.com>
2015-07-19 16:12:51 -0400

    custom product variant price fix

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,17 +1,12 @@
 Spree::Variant.class_eval do
   after_create :add_variant_price, :if => :okay_to_increase_variant_price?
-  after_update :add_variant_price, :if => :okay_to_increase_variant_price?
 
   def add_variant_price
-    puts 'self.is_master = #{self.is_master}'
-    puts 'self.price = #{self.price = product.price + product.price_increase}'
     self.price = product.price + product.price_increase
-    puts 'self.price'
     self.save!
   end
 
   def okay_to_increase_variant_price?
-    !self.is_master? &&
     self.id != product.master.id &&
     product.price_increase &&
     product.price_increase > 0 &&
