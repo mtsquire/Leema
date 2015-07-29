6a80470723ace49999cb262f338d1c3fbc400a8b
msquire <marc.squire@mlb.com>
2015-07-29 12:49:42 -0400

    reverting brandon update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,17 +2,16 @@ class ProfilesController < ApplicationController
   layout "profiles"
   def show  
     @user = User.find_by_display_name(params[:id])
-    # wrap this in the if statement so that it doesnt throw 500 errors when user types in
-    # leemaeats.com/xxxx
+    @supplier = @user.supplier
+    @orders = @user.spree_orders.all
+    if @user.supplier?
+      @products = Spree::Product.joins(:suppliers).where('supplier_id = ?', @supplier.id)
+    # else
+    #   @products = nil
+    end
+
     if @user
       render action: :show
-      @supplier = @user.supplier
-      @orders = @user.spree_orders.all
-      if @user.supplier?
-        @products = Spree::Product.joins(:suppliers).where('supplier_id = ?', @supplier.id)
-      # else
-      #   @products = nil
-      end
     else
       render file: 'public/404', status: 404, format: [:html]
     end
