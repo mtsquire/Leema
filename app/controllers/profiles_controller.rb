5bc304fd0c143bbb2b407351acf9dc1c52e5bbca
Brandon Hay <brandon.a.hay@gmail.com>
2015-07-28 23:09:33 -0400

    fixed bug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,16 +2,17 @@ class ProfilesController < ApplicationController
   layout "profiles"
   def show  
     @user = User.find_by_display_name(params[:id])
-    @supplier = @user.supplier
-    @orders = @user.spree_orders.all
-    if @user.supplier?
-      @products = Spree::Product.joins(:suppliers).where('supplier_id = ?', @supplier.id)
-    # else
-    #   @products = nil
-    end
-
+    # wrap this in the if statement so that it doesnt throw 500 errors when user types in
+    # leemaeats.com/xxxx
     if @user
       render action: :show
+      @supplier = @user.supplier
+      @orders = @user.spree_orders.all
+      if @user.supplier?
+        @products = Spree::Product.joins(:suppliers).where('supplier_id = ?', @supplier.id)
+      # else
+      #   @products = nil
+      end
     else
       render file: 'public/404', status: 404, format: [:html]
     end
