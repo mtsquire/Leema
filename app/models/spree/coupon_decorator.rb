bdf5cd11409908f4005ed34bd423d9c60a0a577d
Brandon Hay <brandon.a.hay@gmail.com>
2015-08-03 23:59:40 -0400

    deleted byebugs

--- app/models/spree/coupon_decorator.rb
+++ app/models/spree/coupon_decorator.rb
@@ -6,10 +6,8 @@ Spree::PromotionHandler::Coupon.class_eval do
     if !promotion.promotion_rules.empty?
       promotion.promotion_rules.each do |i|
         if i.type = "Spree::Promotion::Rules::UserLoggedIn"
-          byebug
           self.error = "You must sign in or sign up to redeem this promotion."
         else
-          byebug
           self.error = Spree.t(:coupon_code_not_eligible)
         end
       end
