408c0d9ebaba4fed6041291b2543dcbf561add7c
Brandon Hay <brandon.a.hay@gmail.com>
2015-10-22 23:38:30 -0400

    remove unneeded files

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++
@@ -1,4 +0,0 @@
-Spree::Admin::GeneralSettingsController.class_eval do
-  include ApplicationHelper
-  before_filter :check_if_leema_admin
-end
--- app/controllers/spree/admin/users_controller_decorator.rb
+++
@@ -1,4 +0,0 @@
-Spree::Admin::UsersController.class_eval do
-  include ApplicationHelper
-  before_filter :check_if_leema_admin
-end
