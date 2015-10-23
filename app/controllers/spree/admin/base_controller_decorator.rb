0c282513954fdc9611788732fe3fbc3b10215351
Brandon Hay <brandon.a.hay@gmail.com>
2015-10-22 23:18:08 -0400

    add logic to base controller to save code

---
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -0,0 +1,18 @@
+Spree::Admin::BaseController.class_eval do
+  before_filter :restrict_access
+
+  def restrict_access
+    byebug
+    if !spree_current_user.leema_admin?
+      allowed_access = ['shipments', 'suppliers', 'products']
+      unless allowed_access.include?(controller_name)
+        respond_to do |format|
+          format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+          format.xml  { head :not_found }
+          format.any  { head :not_found }
+        end
+      end
+    end
+  end
+
+end
