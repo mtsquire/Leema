eb8bb935676b2a2788f5594364e69a8248ff6c3d
Brandon Hay <brandon.a.hay@gmail.com>
2015-10-22 23:44:27 -0400

    bad name

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -2,8 +2,9 @@ Spree::Admin::BaseController.class_eval do
   before_filter :restrict_access
 
   def restrict_access
+    byebug
     if !spree_current_user.leema_admin?
-      allowed_access = ['shipments', 'suppliers', 'products', 'bank_accounts']
+      allowed_access = ['shipments', 'suppliers', 'products', 'supplier_bank_accounts']
       unless allowed_access.include?(controller_name)
         respond_to do |format|
           format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
