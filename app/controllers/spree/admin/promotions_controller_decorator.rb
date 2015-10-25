0cc9f1dbd0c3115c7b5c7668fbed21317b93b3b1
Brandon Hay <brandon.a.hay@gmail.com>
2015-10-25 18:17:55 -0400

    change calculator

--- app/controllers/spree/admin/promotions_controller_decorator.rb
+++ app/controllers/spree/admin/promotions_controller_decorator.rb
@@ -1,18 +1,18 @@
-Spree::Admin::PromotionsController.class_eval do
+# Spree::Admin::PromotionsController.class_eval do
 
-  protected
-  def collection
-    return @collection if defined?(@collection)
-    params[:q] ||= HashWithIndifferentAccess.new
-    params[:q][:s] ||= 'id desc'
+#   protected
+#   def collection
+#     return @collection if defined?(@collection)
+#     params[:q] ||= HashWithIndifferentAccess.new
+#     params[:q][:s] ||= 'id desc'
 
-    @collection = super
-    @search = @collection.ransack(params[:q])
-    @collection = @search.result(distinct: true).
-      includes(promotion_includes).
-      page(params[:page])
+#     @collection = super
+#     @search = @collection.ransack(params[:q])
+#     @collection = @search.result(distinct: true).
+#       includes(promotion_includes).
+#       page(params[:page])
 
-    @collection
-  end
+#     @collection
+#   end
 
-end
+# end
