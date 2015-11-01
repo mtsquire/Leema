985f0bc4d299ab733627dff4763514812d9a814b
Brandon Hay <brandon.a.hay@gmail.com>
2015-11-01 17:22:58 -0500

    test change

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,27 +1,16 @@
-require 'sidekiq'
 require 'autoscaler/sidekiq'
-require 'autoscaler/heroku_scaler'
-
-heroku = nil
-if ENV['HEROKU_APP']
-  heroku = Autoscaler::HerokuScaler.new
-end
+require 'autoscaler/heroku_platform_scaler'
 
 Sidekiq.configure_client do |config|
-  if heroku
-    config.client_middleware do |chain|
-      chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
-    end
+  config.client_middleware do |chain|
+    p 'im doing stuff!'
+    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuPlatformScaler.new
   end
 end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
-    if heroku
-      p "[Sidekiq] Running on Heroku, autoscaler is used"
-      chain.add(Autoscaler::Sidekiq::Server, heroku, 60) # 60 seconds timeout
-    else
-      p "[Sidekiq] Running locally, so autoscaler isn't used"
-    end
+    p 'im doin stuff again!'
+    chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, 60) # 60 second timeout
   end
 end
