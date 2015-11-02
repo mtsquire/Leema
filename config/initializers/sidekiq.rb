0a8e1ae5de82f04dce5ab8fdc66605ee7657660e
Brandon Hay <brandon.a.hay@gmail.com>
2015-11-01 19:36:21 -0500

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,16 @@
-require 'autoscaler/sidekiq'
-require 'autoscaler/heroku_platform_scaler'
+# require 'autoscaler/sidekiq'
+# require 'autoscaler/heroku_platform_scaler'
 
-Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    p 'im doing stuff!'
-    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuPlatformScaler.new
-  end
-end
+# Sidekiq.configure_client do |config|
+#   config.client_middleware do |chain|
+#     p 'im doing stuff!'
+#     chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuPlatformScaler.new
+#   end
+# end
 
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    p 'im doin stuff again!'
-    chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, 60) # 60 second timeout
-  end
-end
+# Sidekiq.configure_server do |config|
+#   config.server_middleware do |chain|
+#     p 'im doin stuff again!'
+#     chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, 60) # 60 second timeout
+#   end
+# end
