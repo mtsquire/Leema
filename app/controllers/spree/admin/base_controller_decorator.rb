92af2d358d3f4b2174f486a5ecdb817b32cf5789
Brandon Hay <brandon.a.hay@gmail.com>
2015-11-07 13:57:35 -0500

    fix for custom order problem

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -3,7 +3,12 @@ Spree::Admin::BaseController.class_eval do
 
   def restrict_access
     if !spree_current_user.leema_admin?
-      allowed_access = ['shipments', 'suppliers', 'products', 'supplier_bank_accounts', 'images']
+      allowed_access = ['shipments',
+                        'suppliers',
+                        'products',
+                        'supplier_bank_accounts',
+                        'images',
+                        'prototypes'] # need prototypes otherwise custom orders wont work
       unless allowed_access.include?(controller_name)
         respond_to do |format|
           format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
