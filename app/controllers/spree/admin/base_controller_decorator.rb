7440e474967aa1c2e55a0ae6f5da02b584d9ad65
Brandon Hay <brandon.a.hay@gmail.com>
2015-11-07 14:04:34 -0500

    add comment

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -3,12 +3,16 @@ Spree::Admin::BaseController.class_eval do
 
   def restrict_access
     if !spree_current_user.leema_admin?
-      allowed_access = ['shipments',
-                        'suppliers',
-                        'products',
-                        'supplier_bank_accounts',
-                        'images',
-                        'prototypes'] # need prototypes otherwise custom orders wont work
+      allowed_access = [
+        'shipments',
+        'suppliers',
+        'products',
+        'supplier_bank_accounts',
+        'images',
+        'prototypes']
+        # need prototypes otherwise products wont populate a variant id and custom orders wont work
+        # this is hacky because we dont want suppliers being able to tamper with the
+        # store/admin/prototypes page and we should find a way to make this more secure
       unless allowed_access.include?(controller_name)
         respond_to do |format|
           format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
