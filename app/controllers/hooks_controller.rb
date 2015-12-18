35659ed88b97c6fd39b0293988020e37fc6a5d16
msquire <marc.squire@mlb.com>
2015-12-17 23:51:34 -0500

    fixing hooks controller

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,7 +4,6 @@ skip_before_filter  :verify_authenticity_token
 
   def stripe
     case params[:result][:status]
-<<<<<<< HEAD
     when 'in_transit'
       @shipment = Spree::Shipment.where(
         transferred: false,
@@ -22,7 +21,7 @@ skip_before_filter  :verify_authenticity_token
         if item.product.shipping_included == 1
           item total += item.product.cost_price * item.quantity
         else
-          item_total += item.product.price * item.quantity
+          item_total += item.variant.price * item.quantity
         end
       end
       transfer = Stripe::Transfer.create(
@@ -34,34 +33,9 @@ skip_before_filter  :verify_authenticity_token
       )
       @shipment.transferred = true
       @shipment.save!
-=======
-      when 'in_transit'
-        @shipment = Spree::Shipment.where(
-          transferred: false,
-          state: "shipped",
-          tracking: params[:result][:tracking_code]
-          ).first
-        if !@shipment
-          puts 'no shipment found'
-          return
-        end
-        item_total = 0
-        @shipment.line_items.each do |item|
-          item_total += item.variant.price * item.quantity
-        end
-        transfer = Stripe::Transfer.create(
-          # Take 10% for ourselves from the total cost
-          # of items per supplier(shipment)
-          :amount => (item_total * (100 - @shipment.supplier.commission_percentage)).floor,
-          :currency => "usd",
-          :recipient => @shipment.supplier.token
-        )
-        @shipment.transferred = true
-        @shipment.save!
-      when 'delivered'
-        @delivered_shipment = Spree::Shipment.find_by_tracking(params[:result][:tracking_code])
-        Spree::ShipmentMailer.order_delivered_email(@delivered_shipment.id).deliver!
->>>>>>> master
+    when 'delivered'
+      @delivered_shipment = Spree::Shipment.find_by_tracking(params[:result][:tracking_code])
+      Spree::ShipmentMailer.order_delivered_email(@delivered_shipment.id).deliver!
     end
   end
 end
